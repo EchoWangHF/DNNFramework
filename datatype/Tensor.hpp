dc960daf906aa2a2a4c9c790ac04a983398349a5
wangjun <1427014893@qq.com>
2019-10-12 00:29:36 +0800

    <feat>(datatype):add the Tensor

---
+++ datatype/Tensor.hpp
@@ -0,0 +1,20 @@
+#ifndef _DNN_TENSOR_
+#define _DNN_TENSOR_
+
+#include<iostream>
+
+namespace DNN{
+
+class Tensor{
+public:
+    Tensor();
+    virtual ~Tensor();
+
+    Tensor CreateTensor(); 
+
+
+};
+
+}
+
+#endif
