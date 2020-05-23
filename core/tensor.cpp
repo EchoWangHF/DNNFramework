711b58fdba1bf9724967ed496e53936dec62ab3f
wangjun <1427014893@qq.com>
2020-05-23 12:32:02 +0800

    feat(core):add some function

--- core/tensor.cpp
+++ core/tensor.cpp
@@ -3,12 +3,25 @@
  * The struct of tensor
  */
 
-#include"tensor.h"
+#include "tensor.h"
 
-// DNN_STATUS CreateTensorDescriptor(TensorDescriptor *desc){
+namespace DNN {
 
-// }
+Tensor::Tensor()
+    : dim(0),
+      shape(),
+      layout(DNN::DNN_LAYOUT_ARRAY),
+      dataType(DNN::DNN_DATATYPE_FLOAT32),
+      device(DNN::CPU),
+      host_addr(nullptr),
+      device_addr(nullptr) {}
 
-// DNN_STATUS Set
+Tensor::Tensor(const Tensor& other):dim(other.getDim()),shape(other.getShape()),
+        layout(other.getLayout()),dataType(other.getDataType()),device(other.getDevice()),
+        host_addr(nullptr),device_addr(nullptr){
+          if(other.getHostAddress()!=nullptr){
+          }
 
+}
 
+}  // namespace DNN
--- core/tensor.h
+++ core/tensor.h
@@ -3,32 +3,70 @@
  * The struct of tensor
  */
 
-#ifndef TINY_DNN_CORE_TENSOR_H
-#define TINY_DNN_CORE_TENSOR_H
+#ifndef DNN_CORE_TENSOR_H
+#define DNN_CORE_TENSOR_H
 
-#include<stdint.h>
-#include<vector>
+#include <stdint.h>
 
-#include"core.h"
-template <typename T>
-class Tensor{
-public:
-    uint32_t dim;
-    std::vector<uint32_t> dims;
-    Layout Layout;
-    DataType dataType;
-    std::share_ptr<void> data;
+#include <memory>
+#include <vector>
+
+#include "core.h"
+
+namespace DNN {
+
+class Tensor {
+ public:
+  explicit Tensor();
+  explicit Tensor(const Tensor& other);
+  Tensor& operator=(const Tensor& other);
+  virtual ~Tensor();
+
+  explicit Tensor(const Layout& layout);
+  explicit Tensor(const std::vector<int>& shape_vec);
+
+  void setDim(const uint32_t& dim);
+  uint32_t getDim() const;
+
+  void setShape(const std::vector<uint32_t>& shape_vec);
+  std::vector<uint32_t> getShape() const;
+
+  void setLayout(const Layout& layout);
+  Layout getLayout() const;
+
+  void setDataType(const DataType& dtype);
+  DataType getDataType() const;
+
+  void setDevice(const Device& device);
+  Device getDevice() const;
+
+  void setHostAddress(const void* ptr);
+  void* getHostAddress() const;
+
+  void setDeviceAddress(const void* ptr);
+  void* getDeviceAddress() const;
+
+ protected:
+  /*tensor dim*/
+  uint32_t dim;
+
+  /*the dimension in each dim*/
+  std::vector<uint32_t> shape;
+
+  /* data layout*/
+  Layout layout;
+
+  /* data datatype*/
+  DataType dataType;
+
+  /* device kind*/
+  Device device;
+
+  /* data ptr in host memory*/
+  void* host_addr;
+
+  /* data ptr in device memory*/
+  void* device_addr;
 };
-
-
-// struct TensorStruct
-// {
-//     uint32_t dim;
-//     std::vector<uint32_t> dims;
-//     Layout layout;
-//     DataType dataType;
-// };
-
-//typedef TensorStruct* TensorDescriptor;
-
-#endif 
+}  // namespace DNN
+#endif
